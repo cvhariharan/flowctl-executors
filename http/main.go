e685027f7e17e1e8bcbc61fece648d709369a7cf
Hariharan <14965074+cvhariharan@users.noreply.github.com>
2026-05-04 15:34:54 +0530

    ci: prepare for release

--- http/main.go
+++ http/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	sdkplugin "github.com/cvhariharan/flowctl/sdk/plugin"
 	goplugin "github.com/hashicorp/go-plugin"
 )
 
+// version is set at build time via -ldflags "-X main.version=..."
+var version = "dev"
+
 func main() {
+	fmt.Fprintf(os.Stderr, "http executor plugin version %s\n", version)
 	goplugin.Serve(&goplugin.ServeConfig{
 		HandshakeConfig: sdkplugin.HandshakeConfig,
 		GRPCServer:      goplugin.DefaultGRPCServer,
